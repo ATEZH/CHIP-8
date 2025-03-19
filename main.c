bf9e9a59204a422a95767628ed2c638d6662e926
ATEZH <arthaxholli@gmail.com>
2025-03-20 00:02:56 +0100

    add write_program_to_memory function

--- main.c
+++ main.c
@@ -17,8 +17,10 @@ void push(struct Stack *stack, uint16_t value);
 void stack_overflow(void);
 void stack_underflow(void);
 
-int main(void) {
-    uint8_t RAM[RAM_SIZE] = { //stores a byte for each pixel
+void write_program_to_memory(char *path, uint8_t *RAM);
+
+int main(int argc, char *argv[]) {
+    uint8_t RAM[RAM_SIZE] = {
             0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
             0x20, 0x60, 0x20, 0x20, 0x70, // 1
             0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
@@ -38,7 +40,7 @@ int main(void) {
     };
     uint8_t display_grid[DISPLAY_WIDTH*DISPLAY_HEIGHT];
     uint16_t I; // index register
-    uint16_t PC; // program counter
+    uint16_t PC = 0x200; // program counter
     uint8_t delay_timer, sound_timer = UINT8_MAX;
     int8_t V0, V1, V2, V3, V4, V5, V6, V7, V8, V9, VA, VB, VC, VD, VE, VF; // general purpose variable registers
     struct Stack stack = {
@@ -46,7 +48,26 @@ int main(void) {
         .top = 0,
     };
 
+    write_program_to_memory(argv[0], RAM + 0x200);
+
     return 0;
+}
+
+void write_program_to_memory(char *path, uint8_t *RAM) {
+    FILE *fp = fopen(path, "rb");
+    if (fp == NULL) {
+        printf("Can't open %s\n", path);
+        exit(EXIT_FAILURE);
+    }
+    while (fread(RAM, sizeof(*RAM), 1, fp) == 1) {
+        RAM++;
+    }
+    if (ferror(fp)) {
+        printf("Error reading from file %s\n", path);
+        fclose(fp);
+        exit(EXIT_FAILURE);
+    }
+    fclose(fp);
 }
 
 void stack_overflow() {
