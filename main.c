245e8d4651cf140719dfc132f1a68a1c7cf132fa
ATEZH <arthaxholli@gmail.com>
2025-03-24 00:57:59 +0100

    implemented timer decrement (roughly decrements at 60Hz) and refactored code so that RAM, I, PC, delay and sound timer, V registers, and stack are contained in a Context struct and passed down to other functions as Context fields or as the struct. Shifted the nibbles at the beginning of decode_execute instead of shifting when passing to instructions

--- main.c
+++ main.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <sys/time.h>
 #include <time.h>
 #include <SDL.h>
 
 #define RAM_SIZE 4096
 #define STACK_SIZE 32
+#define NUM_OF_VREGISTERS 16
 #define DISPLAY_WIDTH 64
 #define DISPLAY_HEIGHT 32
 #define BLOCK_SIZE 20
@@ -15,6 +17,16 @@
 struct Stack {
     uint16_t stack[STACK_SIZE];
     uint8_t top;
+};
+
+struct Context {
+    uint8_t RAM[RAM_SIZE];
+    uint16_t I;
+    uint8_t V[NUM_OF_VREGISTERS];
+    uint16_t PC;
+    struct Stack stack;
+    uint8_t delay_timer;
+    uint8_t sound_timer;
 };
 
 uint16_t pop(struct Stack *stack);
@@ -50,12 +62,66 @@ void draw(uint8_t *display_grid, uint8_t *RAM, uint16_t *I, uint8_t *V, uint8_t 
 void render(SDL_Renderer *renderer, uint8_t *display_grid);
 
 void write_program_to_memory(char *path, uint8_t *RAM);
-void decode_execute(uint16_t opcode, uint16_t *PC, uint16_t *I, uint8_t *V, struct Stack *stack, uint8_t *RAM, uint8_t *display_grid, SDL_Renderer *renderer);
+void write_font_to_memory(uint8_t *RAM);
+void decode_execute(uint16_t opcode, struct Context *ctx, uint8_t *display_grid, SDL_Renderer *renderer);
+void decrement_timers(uint8_t *delay_timer, uint8_t *sound_timer, uint64_t *time_stamp);
 uint16_t keyboard_value(SDL_Event event);
 uint16_t fetch(uint16_t *PC, uint8_t *RAM);
+uint64_t time_in_milliseconds(void);
 
 int main(int argc, char *argv[]) {
-    uint8_t RAM[RAM_SIZE] = {
+    struct Context context = {
+            .RAM = {0},
+            .I = 0,
+            .V = {0},
+            .PC = PROGRAM_START_POSITION,
+            .stack = { .stack = {0}, .top = 0 },
+            .delay_timer = UINT8_MAX,
+            .sound_timer = UINT8_MAX
+    };
+    uint16_t opcode;
+    uint64_t time_stamp;
+
+    uint8_t display_grid[DISPLAY_WIDTH][DISPLAY_HEIGHT];
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_Event event;
+
+    bool close = false;
+    srand(time(NULL) ^ clock() ^ getpid());
+
+    write_font_to_memory(context.RAM);
+    write_program_to_memory(argv[1], context.RAM + PROGRAM_START_POSITION);
+
+    SDL_LogSetPriority(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_DEBUG);
+
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        SDL_Log("SDL_Init Error: %s\n", SDL_GetError());
+        return 1;
+    }
+    window = SDL_CreateWindow("CHIP_8",
+                              SDL_WINDOWPOS_CENTERED,
+                              SDL_WINDOWPOS_CENTERED,
+                              DISPLAY_WIDTH * BLOCK_SIZE, DISPLAY_HEIGHT * BLOCK_SIZE, 0);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+    time_stamp = time_in_milliseconds();
+    while (!close) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) close = true;
+        }
+        decrement_timers(&context.delay_timer, &context.sound_timer, &time_stamp);
+        opcode = fetch(&context.PC, context.RAM);
+        decode_execute(opcode, &context, (uint8_t *) display_grid, renderer);
+    }
+    SDL_Quit();
+    return 0;
+}
+
+void write_font_to_memory(uint8_t *RAM) {
+    uint8_t i;
+    uint8_t font[] = {
             0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
             0x20, 0x60, 0x20, 0x20, 0x70, // 1
             0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
@@ -71,45 +137,20 @@ int main(int argc, char *argv[]) {
             0xF0, 0x80, 0x80, 0x80, 0xF0, // C
             0xE0, 0x90, 0x90, 0x90, 0xE0, // D
             0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
-            0xF0, 0x80, 0xF0, 0x80, 0x80  // F, this is the font
+            0xF0, 0x80, 0xF0, 0x80, 0x80  // F
     };
-    uint16_t I; // index register
-    uint16_t PC = PROGRAM_START_POSITION; // program counter
-    uint16_t opcode;
-    uint8_t delay_timer, sound_timer = UINT8_MAX;
-    uint8_t V[16] = {0};
-    struct Stack stack = { .stack = {0}, .top = 0 };
-    uint8_t display_grid[DISPLAY_WIDTH][DISPLAY_HEIGHT];
-    SDL_Window *window;
-    SDL_Renderer *renderer;
-    SDL_Event event;
-    bool close = false;
-    srand(time(NULL) ^ clock() ^ getpid());
-
-    write_program_to_memory(argv[1], RAM + PROGRAM_START_POSITION);
-
-    SDL_LogSetPriority(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_DEBUG);
-
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-        SDL_Log("SDL_Init Error: %s\n", SDL_GetError());
-        return 1;
+    for (i = 0; i < 80; i++) {
+        *(RAM + i) = *(font + i);
     }
-    window = SDL_CreateWindow("CHIP_8",
-                              SDL_WINDOWPOS_CENTERED,
-                              SDL_WINDOWPOS_CENTERED,
-                              DISPLAY_WIDTH * BLOCK_SIZE, DISPLAY_HEIGHT * BLOCK_SIZE, 0);
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+}
 
-    while (!close) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) close = true;
-        }
-        opcode = fetch(&PC, RAM);
-        decode_execute(opcode, &PC, &I, V, &stack, RAM, (uint8_t *) display_grid, renderer);
+void decrement_timers(uint8_t *delay_timer, uint8_t *sound_timer, uint64_t *time_stamp) {
+    if (time_in_milliseconds() > *time_stamp + 15) {
+        --*delay_timer;
+        --*sound_timer;
+        SDL_Log("%d %d", *delay_timer, *sound_timer);
+        *time_stamp = time_in_milliseconds();
     }
-    SDL_Quit();
-    return 0;
 }
 
 void jump(uint16_t *PC, uint16_t location) {
@@ -256,60 +297,53 @@ void render(SDL_Renderer *render, uint8_t *display_grid) {
     SDL_RenderPresent(render);
 }
 
-void decode_execute(uint16_t opcode,
-                    uint16_t *PC,
-                    uint16_t *I,
-                    uint8_t *V,
-                    struct Stack *stack,
-                    uint8_t *RAM,
-                    uint8_t *display_grid,
-                    SDL_Renderer *renderer) {
-    uint16_t first_nibble = opcode & 0xF000;
-    uint16_t second_nibble = opcode & 0x0F00;
-    uint8_t third_nibble = opcode & 0x00F0;
-    uint8_t fourth_nibble = opcode & 0x000F;
-    switch (first_nibble) {
+void decode_execute(uint16_t opcode, struct Context *ctx, uint8_t *display_grid, SDL_Renderer *renderer) {
+    uint16_t nib1 = opcode & 0xF000; // first nibble
+    uint16_t nib2 = (opcode & 0x0F00) >> 8; // second nibble
+    uint8_t nib3 = (opcode & 0x00F0) >> 4;  // third nibble
+    uint8_t nib4 = opcode & 0x000F;  // fourth nibble
+    switch (nib1) {
         case 0x0000:
-            switch (fourth_nibble) {
+            switch (nib4) {
                 case 0x0:
                     clear_screen(display_grid);
                     render(renderer, display_grid);
                     break;
-                case 0xE: return_from_subroutine(stack, PC); break;
+                case 0xE: return_from_subroutine(&ctx->stack, &ctx->PC); break;
                 default: break;
             } break;
-        case 0x1000: jump(PC, opcode & 0x0FFF); break;
-        case 0x2000: call_subroutine(stack, PC, opcode & 0x0FFF); break;
-        case 0x3000: skip_vx_e_nn(PC, V + (second_nibble >> 8), opcode & 0x00FF); break;
-        case 0x4000: skip_vx_not_e_nn(PC, V + (second_nibble >> 8), opcode & 0x00FF); break;
-        case 0x5000: skip_vx_e_vy(PC, V + (second_nibble >> 8), V + (third_nibble >> 4)); break;
-        case 0x6000: set_v(V + (second_nibble >> 8), opcode & 0x00FF); break;
-        case 0x7000: add_v(V + (second_nibble >> 8), opcode & 0x00FF); break;
+        case 0x1000: jump(&ctx->PC, opcode & 0x0FFF); break;
+        case 0x2000: call_subroutine(&ctx->stack, &ctx->PC, opcode & 0x0FFF); break;
+        case 0x3000: skip_vx_e_nn(&ctx->PC, ctx->V + nib2, opcode & 0x00FF); break;
+        case 0x4000: skip_vx_not_e_nn(&ctx->PC, ctx->V + nib2, opcode & 0x00FF); break;
+        case 0x5000: skip_vx_e_vy(&ctx->PC, ctx->V + nib2, ctx->V + nib3); break;
+        case 0x6000: set_v(ctx->V + nib2, opcode & 0x00FF); break;
+        case 0x7000: add_v(ctx->V + nib2, opcode & 0x00FF); break;
         case 0x8000:
-            switch (fourth_nibble) {
-                case 0x0: load_vy_to_vx(V + (second_nibble >> 8), V + (third_nibble >> 4)); break;
-                case 0x1: or_vx_vy(V + (second_nibble >> 8), V + (third_nibble >> 4)); break;
-                case 0x2: and_vx_vy(V + (second_nibble >> 8), V + (third_nibble >> 4)); break;
-                case 0x3: xor_vx_vy(V + (second_nibble >> 8), V + (third_nibble >> 4)); break;
-                case 0x4: add_vx_vy(V + (second_nibble >> 8), V + (third_nibble >> 4)); break;
-                case 0x5: subtract_vx_vy(V + (second_nibble >> 8), V + (third_nibble >> 4), V + 0x000F); break;
-                case 0x6: shiftr_vx_vy(V + (second_nibble >> 8), V + (third_nibble >> 4), V + 0x000F); break;
-                case 0x7: subtract_vy_vx(V + (second_nibble >> 8), V + (third_nibble >> 4), V + 0x000F); break;
-                case 0xE: shiftl_vx_vy(V + (second_nibble >> 8), V + (third_nibble >> 4), V + 0x000F); break;
+            switch (nib4) {
+                case 0x0: load_vy_to_vx(ctx->V + nib2, ctx->V + nib3); break;
+                case 0x1: or_vx_vy(ctx->V + nib2, ctx->V + nib3); break;
+                case 0x2: and_vx_vy(ctx->V + nib2, ctx->V + nib3); break;
+                case 0x3: xor_vx_vy(ctx->V + nib2, ctx->V + nib3); break;
+                case 0x4: add_vx_vy(ctx->V + nib2, ctx->V + nib3); break;
+                case 0x5: subtract_vx_vy(ctx->V + nib2, ctx->V + nib3, ctx->V + 0x000F); break;
+                case 0x6: shiftr_vx_vy(ctx->V + nib2, ctx->V + nib3, ctx->V + 0x000F); break;
+                case 0x7: subtract_vy_vx(ctx->V + nib2, ctx->V + nib3, ctx->V + 0x000F); break;
+                case 0xE: shiftl_vx_vy(ctx->V + nib2, ctx->V + nib3, ctx->V + 0x000F); break;
                 default: break;
             } break;
-        case 0x9000: skip_vx_not_e_vy(PC, V + (second_nibble >> 8), V + (third_nibble >> 4)); break;
-        case 0xA000: set_i(I, opcode & 0x0FFF); break;
-        case 0xB000: jump_offset(PC, opcode & 0x0FFF, *V); break;
-        case 0xC000: random(V + (second_nibble >> 8), opcode & 0x00FF); break;
+        case 0x9000: skip_vx_not_e_vy(&ctx->PC, ctx->V + nib2, ctx->V + nib3); break;
+        case 0xA000: set_i(&ctx->I, opcode & 0x0FFF); break;
+        case 0xB000: jump_offset(&ctx->PC, opcode & 0x0FFF, *ctx->V); break;
+        case 0xC000: random(ctx->V + nib2, opcode & 0x00FF); break;
         case 0xD000:
-            draw(display_grid, RAM, I, V, second_nibble >> 8, third_nibble >> 4, fourth_nibble);
+            draw(display_grid, ctx->RAM, &ctx->I, ctx->V, nib2, nib3, nib4);
             render(renderer, display_grid);
             break;
         case 0xE000:
-            switch (opcode & 0xFF) {
-                case 0x9E: skip_key_v(V + (second_nibble >> 8), PC);
-                case 0xA1: skip_key_n_v(V + (second_nibble >> 8), PC);
+            switch (opcode & 0x00FF) {
+                case 0x9E: skip_key_v(ctx->V + nib2, &ctx->PC);
+                case 0xA1: skip_key_n_v(ctx->V + nib2, &ctx->PC);
             } break;
         case 0xF000: break;
         default: break;
@@ -343,6 +377,13 @@ uint16_t fetch(uint16_t *PC, uint8_t *RAM) {
     uint16_t instruction = ((uint16_t)(*(RAM + *PC)) << 8) + *(RAM + *PC + 1);
     *PC += 2;
     return instruction;
+}
+
+uint64_t time_in_milliseconds(void) {
+    struct timeval tv;
+
+    gettimeofday(&tv,NULL);
+    return (((uint64_t)tv.tv_sec)*1000)+(tv.tv_usec/1000);
 }
 
 void write_program_to_memory(char *path, uint8_t *RAM) {
